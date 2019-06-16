53a230bdbdcea4d6dc9952e92eba5070996f2e1a
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-06-16 20:26:39 +0200

    updated: make the code more readable

--- dist/observable.js
+++ dist/observable.js
@@ -2,58 +2,50 @@
 const define = Object.defineProperties
 const entries = Object.entries
 
-function on(callbacks, el) {
-  return (event, fn) => {
-    if (callbacks.has(event)) {
-      callbacks.get(event).add(fn)
-    } else {
-      callbacks.set(event, new Set().add(fn))
-    }
-
-    return el
+const on = (callbacks, el) => (event, fn) => {
+  if (callbacks.has(event)) {
+    callbacks.get(event).add(fn)
+  } else {
+    callbacks.set(event, new Set().add(fn))
   }
+
+  return el
 }
 
-function off(callbacks, el) {
-  return (event, fn) => {
-    if (event === ALL_CALLBACKS && !fn) {
-      callbacks.clear()
-    } else {
-      if (fn) {
-        const fns = callbacks.get(event)
+const off = (callbacks, el) => (event, fn) => {
+  if (event === ALL_CALLBACKS && !fn) {
+    callbacks.clear()
+  } else {
+    if (fn) {
+      const fns = callbacks.get(event)
 
-        if (fns) {
-          fns.delete(fn)
-          if (fns.size === 0) callbacks.delete(event)
-        }
-      } else callbacks.delete(event)
-    }
-    return el
+      if (fns) {
+        fns.delete(fn)
+        if (fns.size === 0) callbacks.delete(event)
+      }
+    } else callbacks.delete(event)
   }
+  return el
 }
 
-function one(callbacks, el) {
-  return (event, fn) => {
-    function on(...args) {
-      el.off(event, on)
-      fn.apply(el, args)
-    }
-    return el.on(event, on)
+const one = (callbacks, el) => (event, fn) => {
+  function on(...args) {
+    el.off(event, on)
+    fn.apply(el, args)
   }
+  return el.on(event, on)
 }
 
-function trigger(callbacks, el) {
-  return (event, ...args) => {
-    const fns = callbacks.get(event)
+const trigger = (callbacks, el) => (event, ...args) => {
+  const fns = callbacks.get(event)
 
-    if (fns) fns.forEach(fn => fn.apply(el, args))
+  if (fns) fns.forEach(fn => fn.apply(el, args))
 
-    if (callbacks.get(ALL_CALLBACKS) && event !== ALL_CALLBACKS) {
-      el.trigger.apply(el, [ALL_CALLBACKS, event].concat(args))
-    }
-
-    return el
+  if (callbacks.get(ALL_CALLBACKS) && event !== ALL_CALLBACKS) {
+    el.trigger.apply(el, [ALL_CALLBACKS, event].concat(args))
   }
+
+  return el
 }
 
 const observable = function(el) { // eslint-disable-line
